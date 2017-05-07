32b038a8f7ba59da933abdbdfb4548668c1ade20
Moshe <moshe.waisberg@gmail.com>
2017-05-07 11:23:44 +0300

    main layout with list.

--- app/src/main/java/com/tikalk/moshe/sortedlist/MainActivity.java
+++ app/src/main/java/com/tikalk/moshe/sortedlist/MainActivity.java
@@ -2,12 +2,20 @@ package com.tikalk.moshe.sortedlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends Activity {
+
+    private RecyclerView catalogueList;
+    private CatalogueListAdapter catalogueAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        catalogueList = (RecyclerView) findViewById(android.R.id.list);
+        catalogueAdapter = new CatalogueListAdapter(this);
+        catalogueList.setAdapter(catalogueAdapter);
     }
 }
