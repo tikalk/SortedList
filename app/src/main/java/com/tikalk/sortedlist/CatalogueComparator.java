77a7603892a9b38040cc3434e39dfe824b08a634
Moshe <moshe.waisberg@gmail.com>
2017-05-07 17:46:03 +0300

    remove "moshe" pkg.

--- app/src/main/java/com/tikalk/sortedlist/CatalogueComparator.java
+++ app/src/main/java/com/tikalk/sortedlist/CatalogueComparator.java
@@ -29,7 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.tikalk.moshe.sortedlist;
+package com.tikalk.sortedlist;
 
 import java.util.Comparator;
 
--- app/src/main/java/com/tikalk/sortedlist/CatalogueViewHolder.java
+++ app/src/main/java/com/tikalk/sortedlist/CatalogueViewHolder.java
@@ -29,7 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.tikalk.moshe.sortedlist;
+package com.tikalk.sortedlist;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
